f89b9787f88485dba7c61d6d5a2b124f5824b1cd
Frances Maxwell <fmaxwell@gmail.com>
2023-09-30 01:39:05 +0100

    Losing game logic simplified

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,7 @@ import { WORDS } from '../../data';
 import Banner from '../Banner';
 import GuessInput from '../GuessInput/GuessInput';
 import GuessResults from '../GuessResults/GuessResults';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -21,17 +22,14 @@ function Game() {
 
     if (tentativeGuess.guess.toUpperCase() === answer) {
       setGameStatus('won');
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost');
     }
   };
 
   return (
     <>
-      <GuessResults
-        guesses={guesses}
-        answer={answer}
-        gameStatus={gameStatus}
-        setGameStatus={setGameStatus}
-      />
+      <GuessResults guesses={guesses} answer={answer} />
       <GuessInput addGuess={addGuess} gameStatus={gameStatus} />
       <Banner gameStatus={gameStatus} guesses={guesses} answer={answer} />
     </>
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,7 @@ import Guess from '../Guess';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
-function GuessResults({ guesses, answer, gameStatus, setGameStatus }) {
-  if (guesses.length === 6 && gameStatus !== 'won') {
-    setGameStatus('lost');
-  }
-
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {!!guesses &&
